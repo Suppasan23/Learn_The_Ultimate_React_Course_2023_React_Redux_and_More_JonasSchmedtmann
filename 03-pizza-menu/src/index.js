521587d44978873adf22b51e0742da1908283b98
Suppasan Chaianantakul <117501338+Suppasan23@users.noreply.github.com>
2023-08-14 16:28:38 +0700

    ef56edfde56g

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -1,17 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import  ReactDOM  from "react-dom/client";
 
+export default function Show () {
+    const x = "Benz";
+    return <h1>Hello Alien!!!!!</h1>;
+}
+
+// React v18
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+    <Show/>
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
