185d464c0251d0dc7003e847b6060bb4070fda7e
Suppasan Chaianantakul <117501338+Suppasan23@users.noreply.github.com>
2023-08-15 10:51:10 +0700

    finish 47

--- 00-challenge/src/index.js
+++ 00-challenge/src/index.js
@@ -1,63 +1,72 @@
 import React from "react";
-import  ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import { StrictMode } from "react";
 import "./style.css";
 
-function App(){
-    return (
-        <div className="card">
-            <Avatar image = "avatar/sigma-men.jpg"/>
+function App() {
+  return (
+    <div className="card">
+      <Avatar image="avatar/sigma-men.jpg" />
 
-            <div className="data">
-                <Intro />
-                <SkillList />
-            </div>
-
-        </div>
-    )
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  );
 }
 
-function Avatar(props){
-    return(
-        <>
-            <img className="avatar" src={props.image} alt={props.image.substring(props.image.lastIndexOf('/') + 1)} width={500}></img>
-        </>
-    )
+function Avatar(props) {
+  return (
+    <>
+      <img
+        className="avatar"
+        src={props.image}
+        alt={props.image.substring(props.image.lastIndexOf("/") + 1)}
+        width={500}
+      ></img>
+    </>
+  );
 }
 
-function Intro(){
-    return(
-        <>
-            <h1>Brave Man</h1>
-            <p>A "brave man" refers to an individual who displays courage and fearlessness in the face of challenges, danger, or adversity.</p>
-        </>
-    )
+function Intro() {
+  return (
+    <>
+      <h1>Brave Man</h1>
+      <p>
+        A "brave man" refers to an individual who displays courage and
+        fearlessness in the face of challenges, danger, or adversity.
+      </p>
+    </>
+  );
 }
 
-function SkillList(){
-    return(
-        <div className="skill-list">
-            <Skill skill = "Sword" emoji = "⚔️" color = "#B03A2E"/>
-            <Skill skill = "Axe" emoji = "🪓" color = "#1A5276"/>
-            <Skill skill = "Bow" emoji = "🏹" color = "#0E6655"/>
-            <Skill skill = "Shield" emoji = "🛡️" color = "#B7950B"/>
-            <Skill skill = "Magic" emoji = "☄️" color = "#5D6D7E"/>
-            <Skill skill = "Summon" emoji = "🦄" color = "#A569BD"/>
-        </div>
-    )
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="Sword" emoji="⚔️" color="#B03A2E" />
+      <Skill skill="Axe" emoji="🪓" color="#1A5276" />
+      <Skill skill="Bow" emoji="🏹" color="#0E6655" />
+      <Skill skill="Shield" emoji="🛡️" color="#B7950B" />
+      <Skill skill="Magic" emoji="☄️" color="#5D6D7E" />
+      <Skill skill="Summon" emoji="🦄" color="#A569BD" />
+    </div>
+  );
 }
 
-function Skill(props){
-    return(
-        <p className="skill" style={{backgroundColor : props.color}}>{props.skill} {props.emoji}</p>
-    )
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
 }
-
 
 // Render Zone
-const root = ReactDOM.createRoot(document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <StrictMode>
-        <App />
-    </StrictMode>
-)
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -1,110 +1,122 @@
 import React from "react";
-import  ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 
 const pizzaData = [
-    {
-      name: "Focaccia",
-      ingredients: "Bread with italian olive oil and rosemary",
-      price: 6,
-      photoName: "pizzas/focaccia.jpg",
-      soldOut: false,
-    },
-    {
-      name: "Pizza Margherita",
-      ingredients: "Tomato and mozarella",
-      price: 10,
-      photoName: "pizzas/margherita.jpg",
-      soldOut: false,
-    },
-    {
-      name: "Pizza Spinaci",
-      ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
-      price: 12,
-      photoName: "pizzas/spinaci.jpg",
-      soldOut: false,
-    },
-    {
-      name: "Pizza Funghi",
-      ingredients: "Tomato, mozarella, mushrooms, and onion",
-      price: 12,
-      photoName: "pizzas/funghi.jpg",
-      soldOut: false,
-    },
-    {
-      name: "Pizza Salamino",
-      ingredients: "Tomato, mozarella, and pepperoni",
-      price: 15,
-      photoName: "pizzas/salamino.jpg",
-      soldOut: true,
-    },
-    {
-      name: "Pizza Prosciutto",
-      ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
-      price: 18,
-      photoName: "pizzas/prosciutto.jpg",
-      soldOut: false,
-    },
-  ];
-  
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
 
-function App () {
-    return (
-        <div className="container header">
-            <Header />
-            <Menu />
-            <Footer />
-        </div>
-    );
+function App() {
+  return (
+    <div className="container header">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
 }
 
 function Header() {
-    //const style = {color:'red', fontSize: "25px", textTransform: "uppercase"}
-    const style = {}
-    return (
-        <header className="header">
-            <h1 style={style} >Fast React Pizza Co.</h1>
-        </header>
-        );
+  const style = {};
+  return (
+    <header className="header">
+      <h1 style={style}>Fast React Pizza Co.</h1>
+    </header>
+  );
 }
 
 function Menu() {
-    return (
-        <main className="menu">
-            <h2>Our Menu</h2>
-            {pizzaData.map((eachPizza, index) => (
-                <Pizza 
-                    key={index} 
-                    eachPizza = {eachPizza}
-                />
-            ))}
-        </main>
-    );
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
+
+  return (
+    <main className="menu">
+      <h2>Our Menu</h2>
+
+      {numPizzas > 0 && (
+        <ul className="pizzas">
+          {pizzas.map((pizza, index) => (
+            <Pizza key={index} pizzaObj={pizza} />
+          ))}
+        </ul>
+      )}
+    </main>
+  );
 }
 
 function Pizza(props) {
-    return (    
-        <div className="pizza">
-            <img src={props.eachPizza.photoName} alt={props.eachPizza.name} />
-            <h3>{props.eachPizza.name}</h3>
-            <p>{props.eachPizza.ingredients}</p>
-            <p>{props.eachPizza.price + 0.5}</p>
-        </div>
-    );
+  return (
+    <li className="pizza">
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+      <h3>{props.pizzaObj.name}</h3>
+      <p>{props.pizzaObj.ingredients}</p>
+      <p>{props.pizzaObj.price + 0.5}</p>
+    </li>
+  );
 }
 
 function Footer() {
-    const hour = new Date().getHours();
-    const openHour = 8;
-    const closeHour = 20;
-    const isOpen = (openHour <= hour && hour <= closeHour);
-    return <footer> {hour} {isOpen ? "We're Open": "We're close!!"} </footer>
+  const hour = new Date().getHours();
+  const openHour = 8;
+  const closeHour = 20;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  return (
+    <footer className="footer">
+      {isOpen && (
+        <div className="order">
+          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+          <button className="btn">Order</button>
+        </div>
+      )}
+    </footer>
+  );
 }
 
 // React v18
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
+  <React.StrictMode>
     <App />
-    </React.StrictMode>
+  </React.StrictMode>
 );
