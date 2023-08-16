8b1b758b7f7a64a1e60d42a9033267442feeb2a4
Suppasan Chaianantakul <117501338+Suppasan23@users.noreply.github.com>
2023-08-16 10:28:49 +0700

    Finish 65

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -11,49 +11,63 @@ const messages = [
   "Be extremely rich 🤑",
 ];
 
-
 export default function App() {
-
   const [step, setStep] = useState(0);
-  const [emoji, setEmoji] = useState("")
+  const [emoji, setEmoji] = useState("");
+  const [isOpen, setIsOpen] = useState(true);
 
-  function handlePrevious(){
-    if(step > 0){
-      setStep(step - 1)
+  function handlePrevious() {
+    if (step > 0) {
+      setStep((s) => s - 1);
       setEmoji(emoji.slice(0, emoji.lastIndexOf("💰")));
-    } 
+    }
   }
 
-  function handleNext(){
-    if(step < messages.length -1) {
-      setStep(step + 1)
+  function handleNext() {
+    if (step < messages.length - 1) {
+      setStep((s) => s + 1);
       for (let i = 1; i <= step; i++) {
         setEmoji(emoji.concat("💰"));
       }
     }
   }
 
-  for (let i = 1; i <= messages.length; i++) {
-    <div className={step >= i && "active"}>{i+1}</div>;
-  }
-  
   return (
     <>
-      <div className="steps">
-        <div className="numbers">
-          {
-            messages.map((_, i) => (<div className={step >= i ? "active" : ""}>{i + 1}</div>)) 
-          }
-        </div>
+      <button className="close" onClick={() => setIsOpen((isOp) => !isOp)}>
+        &times;
+      </button>
 
-        <p className="message">
-          Step {step+1}:{messages[step]}{emoji}
-        </p>
-        <div className="buttons">
-          <button style={{ backgroundColor: "#7950f2", color: "#FFFFFF" }} onClick={handlePrevious}>Previous</button>
-          <button style={{ backgroundColor: "#7950f2", color: "#FFFFFF" }} onClick={handleNext}>Next</button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            {messages.map((_, i) => (
+              <div className={step >= i ? "active" : ""} key={i}>
+                {i + 1}
+              </div>
+            ))}
+          </div>
+
+          <p className="message">
+            Step {step + 1}:{messages[step]}
+            {emoji}
+          </p>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#FFFFFF" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#FFFFFF" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
         </div>
-      </div>
-    </>
+      )}
+    </> //for IsOpen
   );
 }
