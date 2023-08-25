d96ce1682fdced20637f2b282ba5ad6c1822eb61
Suppasan Chaianantakul <117501338+Suppasan23@users.noreply.github.com>
2023-08-25 20:29:05 +0700

    Finish 96

--- 06-eat-n-split/src/App.js
+++ 06-eat-n-split/src/App.js
@@ -1,3 +1,5 @@
+import "./index.css";
+
 const initialFriends = [
   {
     id: 118836,
@@ -20,5 +22,40 @@ const initialFriends = [
 ];
 
 export default function App() {
-  return <div>Hello</div>;
+  return (
+    <div className="app">
+      <div className="sidebar">
+      <FriendsList/>
+      </div>
+    </div>
+  )
+}
+
+function FriendsList() {
+  const friends = initialFriends
+
+  return (
+    <>
+      <ul>
+        {friends.map((friend)=>(
+          <Friend friend={friend} key={friend.id}/>
+        ))}
+      </ul>
+    </>
+  )
+}
+
+function Friend({friend}){
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name}/>
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (<p className="red">You owe {friend.name} {Math.abs(friend.balance)}$</p>)}
+      {friend.balance > 0 && (<p className="green">{friend.name} owe You {Math.abs(friend.balance)}$</p>)}
+      {friend.balance === 0 && (<p>You and {friend.name} are even</p>)}
+
+      <button className="button">Select</button>
+    </li>
+  )
 }
