84e3e5ab15be5ed4e94e153214abfe8bb54f590c
Suppasan Chaianantakul <117501338+Suppasan23@users.noreply.github.com>
2023-08-29 23:13:52 +0700

    Finish 120

--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -5,11 +5,27 @@ import ReactDOM from 'react-dom/client';
 
 import StarRating from './StarRating';
 
-////////////////////////////// index.js ← [App.js] //////////////////////////////
+
+////////////////////////////// index.js ← [App.js]        //////////////////////////////
+//////////////////////////////          ← [StarRating.js] //////////////////////////////
+//////////////////////////////          ← [Test.js]       //////////////////////////////
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/*<App />*/}
-    <StarRating maxRating={10}/>
+    <StarRating maxRating={10} messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}/>
+    <StarRating maxRating={10} size={24} color="red" className="test"  defaultRating={5}/>
+    <Test/>
   </React.StrictMode>
 );
+
+
+////////////////////////////// Test ← [StarRating.js] //////////////////////////////
+function Test() {
+
+  return (
+    <div>
+      <StarRating maxRating={10} color="blue" />
+    </div>
+  )
+}
