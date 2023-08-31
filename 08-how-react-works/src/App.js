d2b6dfc6eb78281fa199f0927d1d5a0a9b1684e9
Suppasan Chaianantakul <117501338+Suppasan23@users.noreply.github.com>
2023-08-31 10:13:03 +0700

    Finish 132

--- 08-how-react-works/src/App.js
+++ 08-how-react-works/src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 const content = [
   {
@@ -43,11 +44,10 @@ function Tabbed({ content }) {
       </div>
 
       {activeTab <= 2 ? (
-        <TabContent item={content.at(activeTab)} />
+        <TabContent key={uuidv4()} item={content.at(activeTab)} />
       ) : (
         <DifferentContent />
       )}
-
     </div>
   );
 }
