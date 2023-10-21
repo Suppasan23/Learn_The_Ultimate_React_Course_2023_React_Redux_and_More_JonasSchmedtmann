4979cbda23467e370ab5e7bf043a0b00167290ef
Suppasan Chaianantakul <117501338+Suppasan23@users.noreply.github.com>
2023-10-21 18:55:51 +0700

    Finish 212

--- 11-worldwise/src/components/Map.jsx
+++ 11-worldwise/src/components/Map.jsx
@@ -1,10 +1,8 @@
 import styles from './Map.module.css'
-import { Link } from "react-router-dom";
 
 function Map() {
     return (
         <div className={styles.mapContainer}>
-            <Link to="/">Back</Link>
             Map
         </div>
     )
--- 11-worldwise/src/components/Sidebar.jsx
+++ 11-worldwise/src/components/Sidebar.jsx
@@ -1,22 +1,18 @@
 import styles from "./Sidebar.module.css"
 import AppNav from "./AppNav";
 import Logo from "./Logo";
-import { Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 function Sidebar() {
     return (
         <>
-            <Link to="/">Back</Link>
-
             <div className={styles.sidebar}>
 
                 <Logo />
 
-                
-
                 <AppNav />
 
-                <p>List of cities</p>
+                <Outlet />
 
                 <footer className={styles.footer}>
                     <p className={styles.copyright}>
