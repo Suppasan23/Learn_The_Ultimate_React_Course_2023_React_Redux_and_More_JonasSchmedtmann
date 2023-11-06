3eab525500dde02f815f203d9b8f8e527a9f8a07
Suppasan Chaianantakul <117501338+Suppasan23@users.noreply.github.com>
2023-11-06 14:10:32 +0700

    Finish 227

---
+++ 11-worldwise/src/contexts/CitiesContext.jsx
@@ -0,0 +1,33 @@
+import { createContext, useEffect, useState } from "react";
+
+const CitiesContext = createContext();
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const BASE_URL = "http://localhost:8000";
+
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error loading data...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchCities();
+  }, []);
+
+  return (
+    <CitiesContext.Provider value={{ cities, isLoading }}>
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+export default CitiesProvider;
