df068a0f1bbbfe65315485360a222430533abd91
Suppasan Chaianantakul <117501338+Suppasan23@users.noreply.github.com>
2023-11-07 11:20:46 +0700

    Finish 231

--- 11-worldwise/src/components/Map.jsx
+++ 11-worldwise/src/components/Map.jsx
@@ -2,10 +2,15 @@ import { useNavigate, useSearchParams } from 'react-router-dom';
 import {MapContainer, TileLayer, Marker, Popup} from "react-leaflet"
 import styles from './Map.module.css';
 import { useState } from 'react';
+import { CitiesContext_Using } from '../contexts/CitiesContext';
+import ShowTheFlag from './ShowTheFlag';
 
 function Map() {
 
     const navigate = useNavigate();
+
+    const { cities } = CitiesContext_Using();
+
     const [mapPosition, setMapPosition] = useState([40, 0]);
 
     const [searchParams, setSearchParams] = useSearchParams();
@@ -21,11 +26,16 @@ function Map() {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={mapPosition}>
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-                </Marker>
+
+                {cities.map((city) => (
+                    <Marker key={city.id} position={[city.position.lat, city.position.lng]}>
+                        <Popup>
+                            <ShowTheFlag bool={false} country={city.country}/>
+                            <span>{city.cityName}</span>
+                        </Popup>
+                    </Marker>
+                ))}
+
             </MapContainer>
 
             <button onClick={()=>setSearchParams({lat:50, lng:23})}>Change Position</button>
