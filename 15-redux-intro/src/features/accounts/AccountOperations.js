5289274cc2c421dee4e6e4a22c84e2e82d7a35e9
Suppasan Chaianantakul <117501338+Suppasan23@users.noreply.github.com>
2023-11-16 10:35:48 +0700

    Finish 273

--- 15-redux-intro/src/features/accounts/AccountOperations.js
+++ 15-redux-intro/src/features/accounts/AccountOperations.js
@@ -22,7 +22,7 @@ function AccountOperations() {
     if(!depositAmount) return;
     dispatch(deposit(Number(depositAmount), currency));
     setDepositAmount("");
-    setCurrency("");
+    setCurrency("USD");
   }
 
   function handleWithdrawal(e) {
