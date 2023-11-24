0cfef0a770b04543f4aeee765ac3de6c17d51a63
Suppasan Chaianantakul <117501338+Suppasan23@users.noreply.github.com>
2023-11-24 12:02:25 +0700

    Finish 286

---
+++ 16-fast-react-pizza/src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+    return (
+        <header>
+            <Link to='/'>Fast react pizza Co.</Link>
+            <p>Shrek</p>
+        </header>
+    )
+}
+
+export default Header;
