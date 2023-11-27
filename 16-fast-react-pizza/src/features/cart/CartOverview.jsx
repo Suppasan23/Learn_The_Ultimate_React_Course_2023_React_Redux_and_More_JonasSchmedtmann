1eba204b72a112bdfb1184502b268c056d71c9ce
Suppasan Chaianantakul <117501338+Suppasan23@users.noreply.github.com>
2023-11-27 21:35:19 +0700

    Finish 297

--- 16-fast-react-pizza/src/features/cart/CartOverview.jsx
+++ 16-fast-react-pizza/src/features/cart/CartOverview.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 
 function CartOverview() {
   return (
-    <div>
+    <div className="bg-stone-800 text-neutral-50">
       <hr></hr>
-      <p>
+      <p className="text-stone-300">
         <span>23 pizzas</span>
         <span>$23.45</span>
       </p>
