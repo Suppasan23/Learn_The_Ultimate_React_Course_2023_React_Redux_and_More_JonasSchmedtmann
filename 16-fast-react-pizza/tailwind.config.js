824c7c11a9ea5ec22f9cab32cb1f77b142db9c95
Suppasan Chaianantakul <117501338+Suppasan23@users.noreply.github.com>
2023-11-27 21:18:47 +0700

    Finish 295

---
+++ 16-fast-react-pizza/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    // Add any other files that include your Tailwind CSS classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
