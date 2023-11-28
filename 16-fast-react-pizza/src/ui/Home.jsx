6dad5819fd386be7eba2080c80c85666d02e6b90
Suppasan Chaianantakul <117501338+Suppasan23@users.noreply.github.com>
2023-11-28 14:24:59 +0700

    Fisnish tailwind section

--- 16-fast-react-pizza/src/ui/Home.jsx
+++ 16-fast-react-pizza/src/ui/Home.jsx
@@ -1,10 +1,12 @@
 function Home() {
   return (
     <div>
-      <h1 className="text-center text-xl underline underline-offset-1 font-semibold">
+      <h1 className="text-center text-xl font-semibold underline underline-offset-1">
         The best pizza.
         <br />
-        <span className="text-yellow-700">Straight out of the oven, straight to you.</span>
+        <span className="text-yellow-700">
+          Straight out of the oven, straight to you.
+        </span>
       </h1>
     </div>
   );
--- 16-fast-react-pizza/tailwind.config.js
+++ 16-fast-react-pizza/tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
     // Add any other files that include your Tailwind CSS classes
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
