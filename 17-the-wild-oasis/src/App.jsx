330cdb8d8b9b929aa17da18de958b38368dead06
Suppasan Chaianantakul <117501338+Suppasan23@users.noreply.github.com>
2023-11-30 23:56:21 +0700

    Finish 330

--- 17-the-wild-oasis/src/App.jsx
+++ 17-the-wild-oasis/src/App.jsx
@@ -1,8 +1,44 @@
+import styled from "styled-components";
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+  cursor: pointer;
+  margin: 20px;
+`;
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`;
+
 function App() {
   return (
-    <div>
-      <h3>Hello World!</h3>
-    </div>
+    <StyledApp>
+      <H1>The Wild Oasis</H1>
+
+      <Button onClick={() => alert("Check in")}>Check in</Button>
+      <Button onClick={() => alert("Check Out")}>Check out</Button>
+
+      <Input type="number" placeholder="Number of guests"></Input>
+    </StyledApp>
   );
 }
 
