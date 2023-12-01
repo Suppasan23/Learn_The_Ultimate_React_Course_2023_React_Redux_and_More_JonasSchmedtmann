c01dbe15f9b1c12bc309c344ea3ecddf9403b3f0
Suppasan Chaianantakul <117501338+Suppasan23@users.noreply.github.com>
2023-12-01 18:06:45 +0700

    g51rw6r1g

--- 17-the-wild-oasis/src/App.jsx
+++ 17-the-wild-oasis/src/App.jsx
@@ -2,16 +2,11 @@ import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
+import Heading from "./ui/Heading";
 
 const StyledApp = styled.div`
   background-color: orangered;
   padding: 20px;
-`;
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
 `;
 
 function App() {
@@ -19,11 +14,12 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <H1>The Wild Oasis</H1>
-
-        <Button onClick={() => alert("Check in")}>Check in</Button>
+        <Heading type="h1">The Wild Oasis</Heading>
+        <Heading type="h2">Check in and out</Heading>
+        <Button onClick={() => alert("Check In")}>Check in</Button>
         <Button onClick={() => alert("Check Out")}>Check out</Button>
 
+        <Heading type="h2">Form</Heading>
         <Input type="number" placeholder="Number of guests"></Input>
         <Input type="number" placeholder="Number of guests"></Input>
       </StyledApp>
---
+++ 17-the-wild-oasis/src/ui/Heading.jsx
@@ -0,0 +1,14 @@
+import styled /* , { css } */ from "styled-components";
+
+/* const test = css`
+  text-align: center;
+  ${10 > 5 && "background-color: yellow;"}
+`; */
+
+const Heading = styled.h1`
+  ${(props) => props.type === "h1" && "h2"}
+  font-size: 30px;
+  font-weight: 600;
+`;
+
+export default Heading;
