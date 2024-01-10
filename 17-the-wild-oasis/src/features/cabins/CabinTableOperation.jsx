a5896d41c744d96033e15aef452da5acc67c3376
Suppasan Chaianantakul <117501338+Suppasan23@users.noreply.github.com>
2024-01-10 22:55:37 +0700

    Finish 375

--- 17-the-wild-oasis/src/features/cabins/CabinTableOperation.jsx
+++ 17-the-wild-oasis/src/features/cabins/CabinTableOperation.jsx
@@ -4,7 +4,14 @@ import Filter from "../../ui/Filter";
 function CabinTableOperation() {
   return (
     <TableOperations>
-      <Filter />
+      <Filter
+        filterField="discount"
+        options={[
+          { value: "all", label: "All" },
+          { value: "no-discount", label: "no-discount" },
+          { value: "with-discount", label: "with-discount" },
+        ]}
+      />
     </TableOperations>
   );
 }
