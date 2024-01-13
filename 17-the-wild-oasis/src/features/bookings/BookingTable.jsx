76aa2d7fa842e5e7ddec953b0e512c9fa5a86037
Suppasan Chaianantakul <117501338+Suppasan23@users.noreply.github.com>
2024-01-13 17:15:03 +0700

    Finish 382

--- 17-the-wild-oasis/src/features/bookings/BookingTable.jsx
+++ 17-the-wild-oasis/src/features/bookings/BookingTable.jsx
@@ -34,7 +34,7 @@ function BookingTable() {
         />
 
         <Table.Footer>
-          <Pagination count={5} />
+          <Pagination count={count} />
         </Table.Footer>
       </Table>
     </Menus>
